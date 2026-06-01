2b87f4cd52de9a33359b6a9f3cb90091db80c1c7
assabbour <assabbour@hotmail.com>
2026-06-01 14:38:33 +0200

    Initial commit

---
+++ eLearning_CDA/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  eLearning_CDA
+//
+//  Created by apprenant 108 on 01/06/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ eLearning_CDA/eLearning_CDAApp.swift
@@ -0,0 +1,17 @@
+//
+//  eLearning_CDAApp.swift
+//  eLearning_CDA
+//
+//  Created by apprenant 108 on 01/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct eLearning_CDAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
